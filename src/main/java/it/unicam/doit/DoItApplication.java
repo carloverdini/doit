81d442f508d5fd2406288b94c4f187229e58739a
carlo <carlo@overside.it>
2021-02-26 06:42:49 +0100

    aggiornamento

--- src/main/java/it/unicam/doit/DoItApplication.java
+++ src/main/java/it/unicam/doit/DoItApplication.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DoitApplication {
+public class DoItApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DoitApplication.class, args);
+        SpringApplication.run(DoItApplication.class, args);
     }
 }
